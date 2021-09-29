b5a383fbf2c99c04124dd33cd33ecdc627277067
Rafael Neves <rafael@giver.com.br>
2021-09-29 12:59:11 -0300

    First commit

---
+++ lib/myui.dart
@@ -0,0 +1,11 @@
+library myui;
+
+export 'src/widgets/box_text.dart';
+export 'src/widgets/box_button.dart';
+export 'src/widgets/box_input_field.dart';
+export 'src/widgets/sized_box.dart';
+
+// Colors Export
+export 'src/shared/app_colors.dart';
+export 'src/shared/sizes.dart';
+export 'src/shared/styles.dart';
