e2a08c249c38afc3b12c04f7b6f77402b25f557b
Rafael Neves <rafael@giver.com.br>
2021-09-30 12:57:06 -0300

    TextButtons

--- lib/myui.dart
+++ lib/myui.dart
@@ -4,6 +4,7 @@ export 'src/widgets/box_text.dart';
 export 'src/widgets/box_button.dart';
 export 'src/widgets/box_input_field.dart';
 export 'src/widgets/sized_box.dart';
+export 'src/widgets/box_text_button_vertical.dart';
 
 // Colors Export
 export 'src/shared/app_colors.dart';
